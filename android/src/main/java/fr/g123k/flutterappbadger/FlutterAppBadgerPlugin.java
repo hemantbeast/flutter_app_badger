52b3553657502dfed978120cc0febb3af41f282a
Junaid Rehmat <junaid.rehmat@zameen.com>
2022-03-31 00:39:35 +0500

    allow user to call getAppBadgeCount from Android as well.

--- android/src/main/java/fr/g123k/flutterappbadger/FlutterAppBadgerPlugin.java
+++ android/src/main/java/fr/g123k/flutterappbadger/FlutterAppBadgerPlugin.java
@@ -9,6 +9,8 @@ import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
 import io.flutter.plugin.common.MethodCall;
+import android.preference.PreferenceManager;
+import android.content.SharedPreferences;
 import me.leolin.shortcutbadger.ShortcutBadger;
 
 /**
@@ -39,18 +41,38 @@ public class FlutterAppBadgerPlugin implements MethodCallHandler, FlutterPlugin 
 
   @Override
   public void onMethodCall(MethodCall call, @NonNull Result result) {
+    SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(applicationContext);
+    int count = prefs.getInt("flutter_app_badger_count_key", 0);
+  
     switch (call.method) {
       case "updateBadgeCount":
+        count = count + 1;
         ShortcutBadger.applyCount(applicationContext, Integer.valueOf(call.argument("count").toString()));
+
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putInt("flutter_app_badger_count_key", count);
+        editor.commit();
         result.success(null);
         break;
+
       case "removeBadge":
         ShortcutBadger.removeCount(applicationContext);
+        count = 0;
+
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putInt("flutter_app_badger_count_key", count);
+        editor.commit();
         result.success(null);
         break;
+
       case "isAppBadgeSupported":
         result.success(ShortcutBadger.isBadgeCounterSupported(applicationContext));
         break;
+
+      case "getAppBadgeCount":
+        result.success(count);
+        break;
+
       default:
         result.notImplemented();
         break;
